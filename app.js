5b87773c51121b4e956fa91050d804ca0e02d0c6
NivinCNC <nivincharles@gmail.com>
2025-06-12 06:39:10 +0530

    update headers

--- app.js
+++ app.js
@@ -16,6 +16,7 @@ app.use(
             "Accept-Encoding": "gzip, deflate",
             "Accept-Language": "en-US,en;q=0.9",
             "Connection": "keep-alive",
+            "priority": "u=0, i",
             "Host": "loane359jil.com",
             "Sec-Fetch-Dest": "document",
             "Sec-Fetch-Mode": "navigate",
